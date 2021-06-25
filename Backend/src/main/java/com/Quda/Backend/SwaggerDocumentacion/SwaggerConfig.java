d4572da05d0473a9ff7fb884957300f3c34d0053
Luis Herrera García <cookie30.ld@gmail.com>
2021-06-25 13:22:04 -0500

    BACK - Se organiza el proyecto
    se agrega orden para las nuevas implementaciones y anexo a ello la libreria de seguridad de spring.

--- Backend/src/main/java/com/Quda/Backend/SwaggerDocumentacion/SwaggerConfig.java
+++ Backend/src/main/java/com/Quda/Backend/SwaggerDocumentacion/SwaggerConfig.java
@@ -14,7 +14,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
-        .select().apis(RequestHandlerSelectors.basePackage("com.Quda.Backend.Controladores"))
+        .select().apis(RequestHandlerSelectors.basePackage("com.Quda.Backend.TiendaApp.Controladores"))
                 .build();
     }
 }
